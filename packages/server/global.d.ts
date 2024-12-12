7373c64e93547076af514f2f9014315d63fff6f2
伊墨 <1905672206@qq.com>
2024-12-12 11:04:37 +0800

    chore: 添加环境变量文件

---
+++ packages/server/global.d.ts
@@ -0,0 +1,6 @@
+declare namespace NodeJS {
+  interface ProcessEnv {
+    readonly ACCESS_KEY_ID: string
+    readonly ACCESS_KEY_SECRED: string
+  }
+}
--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -1,4 +1,21 @@
-import * as express from 'express'
+import express from 'express'
+import dotenv from 'dotenv'
+import { resolve as _resolve } from 'node:path'
+
+function resolve(...paths: string[]) {
+  return _resolve(__dirname, ...paths)
+}
+
+function loadEnvFile() {
+  const paths = ['../../../.env', '../../../.env.local'].map((path) => resolve(path))
+  dotenv.config({ path: paths, override: true })
+}
+
+function main() {
+  loadEnvFile()
+}
+
+main()
 
 const router = express.Router()
 
