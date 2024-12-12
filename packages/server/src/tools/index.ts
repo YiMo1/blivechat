ecec154e45a0659a224a0142df4b987c5eff553a
伊墨 <1905672206@qq.com>
2024-12-12 15:18:16 +0800

    chore(server): 添加api/index.ts文件

--- packages/server/src/tools/index.ts
+++ packages/server/src/tools/index.ts
@@ -1,9 +1,4 @@
 import crypto from 'crypto'
-import axios from 'axios'
-
-axios.defaults.headers.common.Accept = 'application/json'
-axios.defaults.headers.common['Content-Type'] = 'application/json'
-axios.defaults.baseURL = 'https://live-open.biliapi.com'
 
 export function getMd5Content(data: crypto.BinaryLike) {
   return crypto.createHash('md5').update(data).digest('hex')
