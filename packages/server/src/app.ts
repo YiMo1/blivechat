bc581c14b7e38ca699d311095c90e538f88ae1e0
伊墨 <1905672206@qq.com>
2024-12-12 19:20:43 +0800

    feat(server): 添加跨域处理

--- packages/server/src/app.ts
+++ packages/server/src/app.ts
@@ -16,6 +16,12 @@ function main() {
   loadEnvFile()
   const app = express()
   app.use(express.json())
+  app.use((_, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Headers', '*')
+    res.header('Access-Control-Allow-Methods', '*')
+    next()
+  })
   app.use(router)
   app.listen(3000)
 }
