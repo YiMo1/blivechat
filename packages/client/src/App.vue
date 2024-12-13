1c8a0022b3bd9712a245d79c6d6f34e62b8e4631
伊墨 <1905672206@qq.com>
2024-12-13 15:46:10 +0800

    feat(client): 完成直播间长连接步骤

--- packages/client/src/App.vue
+++ packages/client/src/App.vue
@@ -5,38 +5,59 @@
 
 <script setup lang="ts">
 import { ref } from 'vue'
+import {
+  INFO_SESSION_STORAGE_KEY,
+  PROJECT_HEARBEAT_INTERVAL,
+  WS_HEARBEAT_INTERVAL,
+  makePacket,
+  OPERATION,
+  parseWsMessage,
+} from './tool/index.ts'
+import { useSessionStorage, StorageSerializers, useWebSocket, useIntervalFn } from '@vueuse/core'
 import { startGame, keepHeartbeat, endGame, type Info } from './api/index.ts'
 
 const code = ref('')
-const info = ref<Info>()
-const id = ref<number>()
+const info = useSessionStorage<Info | null>(INFO_SESSION_STORAGE_KEY, null, { serializer: StorageSerializers.object })
+const { pause: pauseHearbeat, resume: resumeHearbeat } = useIntervalFn(heartbeat, PROJECT_HEARBEAT_INTERVAL, {
+  immediate: false,
+})
+const { close: closeWebsocket } = useWebSocket(() => info.value?.websocket_info.wss_link[0], {
+  heartbeat: { interval: WS_HEARBEAT_INTERVAL, message: makePacket('', OPERATION.OP_HEARTBEAT) },
+  immediate: false,
+  onConnected(ws) {
+    ws.send(makePacket(info.value!.websocket_info.auth_body, OPERATION.OP_AUTH))
+  },
+  async onMessage(_, event) {
+    const data = await parseWsMessage(event.data)
+    console.log(data)
+  },
+})
 
 async function start() {
-  console.log('=========准备开始===========')
   const { data } = await startGame(code.value)
   if (data.code !== 0) {
     throw new Error(data.message)
   }
-  console.log('=========成功开始===========')
   info.value = data.data
-  id.value = window.setInterval(async () => {
-    console.log('=========准备心跳===========')
-    const { data } = await keepHeartbeat(info.value!.game_info.game_id)
-    if (data.code !== 0) {
-      throw new Error(data.message)
-    }
-    console.log('=========成功心跳===========')
-  }, 1000 * 20)
+  resumeHearbeat()
+}
+
+async function heartbeat() {
+  if (!info.value) return
+  const { data } = await keepHeartbeat(info.value.game_info.game_id)
+  if (data.code !== 0) {
+    throw new Error(data.message)
+  }
 }
 
 async function end() {
   if (!info.value) return
-  console.log('=========准备结束===========')
   const { data } = await endGame(info.value.game_info.game_id)
   if (data.code !== 0) {
     throw new Error(data.message)
   }
-  clearInterval(id.value)
-  console.log('=========成功结束===========')
+  pauseHearbeat()
+  closeWebsocket()
+  info.value = null
 }
 </script>
---
+++ packages/client/src/tool/index.ts
@@ -0,0 +1,89 @@
+import { isString } from 'shared'
+import { inflate } from 'pako'
+
+export const INFO_SESSION_STORAGE_KEY = 'BLIVECHAT_INFO'
+export const PROJECT_HEARBEAT_INTERVAL = 1000 * 20
+export const WS_HEARBEAT_INTERVAL = 1000 * 30
+export const HEADER_SIZE = 16
+
+export enum OPERATION {
+  OP_HEARTBEAT = 2,
+  OP_HEARTBEAT_REPLY = 3,
+  OP_SEND_SMS_REPLY = 5,
+  OP_AUTH = 7,
+  OP_AUTH_REPLY = 8,
+}
+
+export enum VERSION {
+  ACTUAL = 0,
+  COMPRESSED = 2,
+}
+
+export enum CMD {
+  CHAT = 'LIVE_OPEN_PLATFORM_DM',
+  GIFT = 'LIVE_OPEN_PLATFORM_SEND_GIFT',
+  SUPER_CHAT = 'LIVE_OPEN_PLATFORM_SUPER_CHAT',
+  SUPER_CHAT_OFFLINE = 'LIVE_OPEN_PLATFORM_SUPER_CHAT_DEL',
+  GUARD = 'LIVE_OPEN_PLATFORM_GUARD',
+  LIKE = 'LIVE_OPEN_PLATFORM_LIKE',
+  ENTER_ROOM = 'LIVE_OPEN_PLATFORM_LIVE_ROOM_ENTER',
+  LIVE_START = 'LIVE_OPEN_PLATFORM_LIVE_START',
+  LIVE_END = 'LIVE_OPEN_PLATFORM_LIVE_END',
+  INTERACTION_END = 'LIVE_OPEN_PLATFORM_INTERACTION_END',
+}
+
+const textEncoder = new TextEncoder()
+
+export function makePacket(rawBody: object | string, operation: OPERATION, version: VERSION = VERSION.ACTUAL) {
+  const body = textEncoder.encode(isString(rawBody) ? rawBody : JSON.stringify(rawBody))
+  const headerBuffer = new ArrayBuffer(HEADER_SIZE)
+  const headerView = new DataView(headerBuffer)
+  headerView.setUint32(0, HEADER_SIZE + body.byteLength)
+  headerView.setUint16(4, HEADER_SIZE)
+  headerView.setUint16(6, version)
+  headerView.setUint32(8, operation)
+  headerView.setUint32(12, 0)
+
+  const header = new Uint8Array(headerBuffer)
+  const packet = new Uint8Array(header.length + body.length)
+  packet.set(header)
+  packet.set(body, header.length)
+  return packet
+}
+
+export async function parseWsMessage(rawData: Blob) {
+  const buffer = await rawData.arrayBuffer()
+  const view = new DataView(buffer)
+  const bodyBuffer = buffer.slice(16)
+  const version = view.getUint16(6) as VERSION
+
+  return {
+    packetLength: view.getUint32(0),
+    headerLength: view.getUint16(4),
+    version: version,
+    operation: view.getUint32(8),
+    sequenceId: view.getUint32(12),
+    body: parseWsMessageBody(bodyBuffer, version),
+  }
+}
+
+const textDecoder = new TextDecoder()
+
+export function parseWsMessageBody(body: ArrayBuffer, version: VERSION): any {
+  if (version === VERSION.ACTUAL) {
+    return JSON.parse(textDecoder.decode(body))
+  }
+  if (version === VERSION.COMPRESSED) {
+    let buffer = inflate(body).buffer
+    const pkgs: Blob[] = []
+    while (buffer.byteLength > 0) {
+      const view = new DataView(buffer)
+      const packetLength = view.getUint32(0)
+      const pkg = buffer.slice(0, packetLength)
+      buffer = buffer.slice(packetLength)
+      pkgs.push(new Blob([pkg]))
+    }
+    return pkgs.map((pkg) => parseWsMessage(pkg))
+  }
+  throw new Error('未知的version值')
+}
