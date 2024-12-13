3b649cc13e38ac8ff48876978410383146ef9211
伊墨 <1905672206@qq.com>
2024-12-13 19:18:56 +0800

    style: eslint支持vue文件解析

--- eslint.config.js
+++ eslint.config.js
@@ -3,14 +3,27 @@ import eslint from '@eslint/js'
 import tseslint from 'typescript-eslint'
 import prettierPlugin from 'eslint-plugin-prettier/recommended'
 import prettierConfig from 'eslint-config-prettier'
+import vuePlugin from 'eslint-plugin-vue'
+import vueEslintParser from 'vue-eslint-parser'
+import tsEslintParser from '@typescript-eslint/parser'
 
 export default tseslint.config(
   { ignores: ['node_modules/*', 'temp/*', '**/dist'] },
   eslint.configs.recommended,
   tseslint.configs.strict,
   tseslint.configs.stylistic,
+  ...vuePlugin.configs['flat/recommended'],
   prettierPlugin,
   prettierConfig,
+  {
+    files: ['**/*.vue', '*.vue'],
+    languageOptions: {
+      parser: vueEslintParser,
+      parserOptions: {
+        parser: tsEslintParser,
+      },
+    },
+  },
   {
     rules: {
       '@typescript-eslint/no-explicit-any': 'off',
