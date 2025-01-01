b8589ba33dcb08422faf4e9682c9c70648bb5314
伊墨 <1905672206@qq.com>
2025-01-01 19:43:49 +0800

    chore: 删除src/components/global.d.ts文件

--- src/component/global.d.ts
+++
@@ -1,7 +0,0 @@
-declare module 'vue' {
-  export interface GlobalComponents {
-    EmojiText: typeof import('./EmojiText.vue').default
-  }
-}
-
-export {}
--- src/component/index.ts
+++ src/component/index.ts
@@ -1,1 +1,7 @@
 export { default as EmojiText } from './EmojiText.vue'
+
+declare module 'vue' {
+  export interface GlobalComponents {
+    EmojiText: typeof EmojiText
+  }
+}
