56bab7cac5925ffaacac7a73a66e966ef7a81669
伊墨 <1905672206@qq.com>
2025-01-03 19:01:32 +0800

    refactor: 更改路由结构

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,14 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 
 const routes: readonly RouteRecordRaw[] = [
-  { path: '/chat', component: () => import('../view/chat/Index.vue') },
-  { path: '/guard', component: () => import('../view/guard/Index.vue') },
+  {
+    path: '/room',
+    component: () => import('../view/room/Index.vue'),
+    children: [
+      { path: 'chat', component: () => import('../view/room/chat/Index.vue') },
+      { path: 'guard', component: () => import('../view/room/guard/Index.vue') },
+    ],
+  },
 ]
 
 export const router = createRouter({
---
+++ src/view/room/Index.vue
@@ -0,0 +1,155 @@
+<template>
+  <router-view />
+</template>
+
+<script setup lang="ts">
+import { useSessionStorage, StorageSerializers, useWebSocket, useIntervalFn, useDocumentVisibility } from '@vueuse/core'
+import { Server } from 'mock-socket'
+import { onBeforeMount, provide, computed, readonly, watchEffect } from 'vue'
+
+import { startGame, keepHeartbeat, type Info } from '@/api/index.ts'
+import {
+  INFO_SESSION_STORAGE_KEY,
+  PROJECT_HEARBEAT_INTERVAL,
+  WS_HEARBEAT_INTERVAL,
+  makePacket,
+  OPERATION,
+  parseWsMessage,
+  CONFIG_INJECTION_KEY,
+  GUARD_SKIN,
+  CHAT_SKIN,
+  DEFUALT_CHAT_RETAINED_QUANTITY,
+  emitter,
+  LIVE_OPEN_PLATFORM_MSG,
+  mockChat,
+  mockGift,
+  mockGuard,
+  mockSuperChat,
+  isString,
+  randomByArray,
+  includesByArray,
+} from '@/tool/index.ts'
+
+const urlSearchParams = new URLSearchParams(window.location.search)
+
+const info = useSessionStorage<Info | null>(INFO_SESSION_STORAGE_KEY, null, { serializer: StorageSerializers.object })
+const visibility = useDocumentVisibility()
+const searchParams = readonly({
+  test: urlSearchParams.get('test'),
+  guardSkin: urlSearchParams.get('guardSkin'),
+  chatSkin: urlSearchParams.get('chatSkin'),
+  chatRetainedQuantity: urlSearchParams.get('chatRetainedQuantity'),
+  code: urlSearchParams.get('code'),
+})
+const isTest = searchParams.test === '1'
+const wsUrl = computed(() => (isTest ? `ws://${location.host}` : info.value?.websocket_info.wss_link[0]))
+
+provide(CONFIG_INJECTION_KEY, readonly(getConfig()))
+
+function getConfig() {
+  const guardSkin = includesByArray(Object.values(GUARD_SKIN), searchParams.guardSkin)
+    ? searchParams.guardSkin
+    : GUARD_SKIN.DEFAULT
+
+  const chatSkin = includesByArray(Object.values(CHAT_SKIN), searchParams.chatSkin)
+    ? searchParams.chatSkin
+    : CHAT_SKIN.DEFAULT
+
+  let chatRetainedQuantity = parseInt(urlSearchParams.get('chatRetainedQuantity') || '')
+  if (Number.isNaN(chatRetainedQuantity) || chatRetainedQuantity < 1) {
+    chatRetainedQuantity = DEFUALT_CHAT_RETAINED_QUANTITY
+  }
+
+  return {
+    guardSkin,
+    chatSkin,
+    chatRetainedQuantity,
+  }
+}
+
+onBeforeMount(async () => {
+  if (isTest) {
+    const mockServer = new Server(wsUrl.value!)
+    mockServer.on('connection', (socket) => {
+      const mockFns = [mockChat, mockGift, mockSuperChat]
+
+      const { pause: pauseMockGuard, resume: resumeMockGuard } = useIntervalFn(() => {
+        const pkg = makePacket(mockGuard(), OPERATION.OP_SEND_SMS_REPLY)
+        socket.send(new Blob([pkg]))
+      }, 1000 * 7)
+
+      const { pause: pauseMockChats, resume: resumeMockChats } = useIntervalFn(() => {
+        const pkg = makePacket(randomByArray(mockFns)(), OPERATION.OP_SEND_SMS_REPLY)
+        socket.send(new Blob([pkg]))
+      }, 1000 / 1)
+
+      function resume() {
+        resumeMockChats()
+        resumeMockGuard()
+      }
+
+      function pause() {
+        pauseMockGuard()
+        pauseMockChats()
+      }
+
+      watchEffect(() => {
+        visibility.value ? resume() : pause()
+      })
+    })
+  } else if (searchParams.code) {
+    const code = isString(searchParams.code) ? searchParams.code : searchParams.code[0]
+
+    async function heartbeat() {
+      if (!info.value) return
+      const { data } = await keepHeartbeat(info.value.game_info.game_id)
+      if (data.code !== 0) {
+        ElMessage.error(data.message)
+        switch (data.code) {
+          case 7003:
+            info.value = null
+            start()
+        }
+        return
+      }
+    }
+
+    async function start() {
+      const { data } = await startGame(code)
+      if (data.code !== 0) {
+        ElMessage.error(data.message)
+        return
+      }
+      info.value = data.data
+    }
+
+    const { resume } = useIntervalFn(heartbeat, PROJECT_HEARBEAT_INTERVAL, {
+      immediate: false,
+    })
+
+    !info.value ? await start() : heartbeat()
+    resume()
+  } else {
+    ElMessage.error('加载失败: 缺少身份码或不是测试环境')
+    return
+  }
+
+  useWebSocket(wsUrl, {
+    heartbeat: isTest ? undefined : { interval: WS_HEARBEAT_INTERVAL, message: makePacket('', OPERATION.OP_HEARTBEAT) },
+    onConnected(ws) {
+      if (!isTest) {
+        ws.send(makePacket(info.value!.websocket_info.auth_body, OPERATION.OP_AUTH))
+      }
+    },
+    async onMessage(_, event) {
+      const data = await parseWsMessage(event.data)
+      if (data.operation === OPERATION.OP_SEND_SMS_REPLY) {
+        const message = data.body
+        emitter.emit(LIVE_OPEN_PLATFORM_MSG, message)
+        emitter.emit(message.cmd, message)
+        return
+      }
+    },
+  })
+})
+</script>
